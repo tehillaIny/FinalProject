75aae824b8811a8e5b7b67707160d7b8c8c3a328
tehillainy <tehilla.iny@gmail.com>
2024-08-27 17:25:32 +0300

    fix image display in the main nav

--- app/src/main/java/com/example/finalproject/MainActivityApp.kt
+++ app/src/main/java/com/example/finalproject/MainActivityApp.kt
@@ -14,14 +14,16 @@ import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import com.example.finalproject.databinding.ActivityMainAppBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import android.content.Intent
-
+import com.bumptech.glide.Glide
 
 class MainActivityApp : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainAppBinding
     private lateinit var navController: NavController
     private lateinit var auth: FirebaseAuth
+    private lateinit var storage: FirebaseStorage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +32,7 @@ class MainActivityApp : AppCompatActivity() {
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
 
         // Initialize NavController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as? NavHostFragment
@@ -41,10 +44,11 @@ class MainActivityApp : AppCompatActivity() {
             NavigationUI.setupWithNavController(bottomNavigationView, navController)
         }
 
+        loadProfileImage()
         // Handle profile image click to show popup menu
         binding.profileImageView.setOnClickListener {
             showProfilePopupMenu(it)
-        }
+            }
     }
 
     private fun showProfilePopupMenu(view: View) {
@@ -64,6 +68,27 @@ class MainActivityApp : AppCompatActivity() {
             }
         }
         popupMenu.show()
+    }
+
+    private fun loadProfileImage() {
+        val user = auth.currentUser
+        if (user != null) {
+            val userId = user.uid
+            val storageRef = storage.reference.child("profile_images/$userId.jpg")
+
+            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                Glide.with(this)
+                    .load(uri)
+                    .placeholder(R.drawable.profile2) // Placeholder image while loading
+                    .circleCrop() // Ensures the image is cropped into a circle
+                    .into(binding.profileImageView)
+            }.addOnFailureListener {
+                // Handle the error if the image retrieval fails
+                binding.profileImageView.setImageResource(R.drawable.profile2) // Default image
+            }
+        } else {
+            binding.profileImageView.setImageResource(R.drawable.profile2) // Default image
+        }
     }
 
         private fun logout() {

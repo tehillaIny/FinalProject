a4280a83efd36254e10ec0cd3b5e9568b07989d6
EllaEyal <ellaeyal1@gmail.com>
2024-09-04 17:16:30 +0300

    design changes

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -13,6 +13,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Set the custom action bar
+        supportActionBar?.apply {
+            displayOptions = androidx.appcompat.app.ActionBar.DISPLAY_SHOW_CUSTOM
+            setCustomView(R.layout.custom_action_bar)
+        }
+
+
         // Ensure this ID matches the one in your activity_main.xml
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_auth) as? NavHostFragment
